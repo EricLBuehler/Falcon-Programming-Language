7d59fac51004b3645d6f32e4b1a505319cae1b2c
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-07-16 10:09:41 -0400

    Add files via upload

---
+++ vm/binary_ops/simple.cpp
@@ -0,0 +1,40 @@
+void binop_add(struct vm* vm, object* left, object* right){
+    object* ret=object_add(left, right);
+    if (ret!=NULL){
+        add_dataframe(vm, vm->objstack, ret);
+    }
+    else{
+        vm_add_err(vm, "TypeError: Invalid operand type for +: '%s', and '%s'.", left->type->name->c_str(), right->type->name->c_str());
+    }
+}
+
+void binop_sub(struct vm* vm, object* left, object* right){
+    object* ret=object_sub(left, right);
+    if (ret!=NULL){
+        add_dataframe(vm, vm->objstack, ret);
+    }
+    else{
+        vm_add_err(vm, "TypeError: Invalid operand type for -: '%s', and '%s'.", left->type->name->c_str(), right->type->name->c_str());
+    }
+}
+
+void binop_mul(struct vm* vm, object* left, object* right){
+    object* ret=object_mul(left, right);
+    if (ret!=NULL){
+        add_dataframe(vm, vm->objstack, ret);
+    }
+    else{
+        vm_add_err(vm, "TypeError: Invalid operand type for *: '%s', and '%s'.", left->type->name->c_str(), right->type->name->c_str());
+    }
+}
+
+void binop_div(struct vm* vm, object* left, object* right){
+    object* ret=object_div(left, right);
+    if (ret!=NULL){
+        add_dataframe(vm, vm->objstack, ret);
+    }
+    else{
+        vm_add_err(vm, "TypeError: Invalid operand type for /: '%s', and '%s'.", left->type->name->c_str(), right->type->name->c_str());
+    }
+}
+
