b88e80847fc5b576e77e86eb9be1c4cccc603319
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-07-28 03:55:14 -0400

    Added EE operator

--- vm/binary_ops/complex.cpp
+++ vm/binary_ops/complex.cpp
@@ -1,9 +1,9 @@
 void binop_is(struct vm* vm, object* left, object* right){
     object* ret=left==right ? new_bool_true() : new_bool_false();
-    if (ret!=NULL){
-        add_dataframe(vm, vm->objstack, ret);
-    }
-    else{
-        vm_add_err(vm, "TypeError: Invalid operand type for +: '%s', and '%s'.", left->type->name->c_str(), right->type->name->c_str());
-    }
+    add_dataframe(vm, vm->objstack, ret);
+}
+
+void binop_ee(struct vm* vm, object* left, object* right){
+    object* ret=object_cmp(left, right, CMP_EQ);
+    add_dataframe(vm, vm->objstack, ret);
 }
