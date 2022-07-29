4a2417ad27c19349f156a1b9d451782e0a9351d6
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-07-29 21:21:08 +0200

    Updated usage of vm_add_err

--- vm/unary_ops/unary.cpp
+++ vm/unary_ops/unary.cpp
@@ -1,6 +1,6 @@
 void unary_neg(struct vm* vm, object* right){
     if (right->type->slot_number==NULL || right->type->slot_number->slot_sub==NULL){
-        vm_add_err(vm, "TypeError: Invalid unary operand -: '%s'.", right->type->name->c_str());
+        vm_add_err(TypeError, vm, "Invalid unary operand -: '%s'.", right->type->name->c_str());
         return;
     }
     object* ret=right->type->slot_number->slot_neg(right);
@@ -8,6 +8,6 @@ void unary_neg(struct vm* vm, object* right){
         add_dataframe(vm, vm->objstack, ret);
     }
     else{
-        vm_add_err(vm, "TypeError: Invalid unary operand -: '%s'.", right->type->name->c_str());
+        vm_add_err(TypeError, vm, "Invalid unary operand -: '%s'.", right->type->name->c_str());
     }
 }
