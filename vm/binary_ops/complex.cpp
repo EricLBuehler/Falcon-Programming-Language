4bdbbf06f08cb2175940517c1503e8d164654d6d
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-08-17 12:49:43 -0400

    Delete vm/binary_ops directory

--- vm/binary_ops/complex.cpp
+++
@@ -1,9 +0,0 @@
-void binop_is(struct vm* vm, object* left, object* right){
-    object* ret=left==right ? new_bool_true() : new_bool_false();
-    add_dataframe(vm, vm->objstack, ret);
-}
-
-void binop_ee(struct vm* vm, object* left, object* right){
-    object* ret=object_cmp(left, right, CMP_EQ);
-    add_dataframe(vm, vm->objstack, ret);
-}
