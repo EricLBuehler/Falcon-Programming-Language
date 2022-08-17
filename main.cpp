935c94826d6aa99a409f7b5ab78abf07cd73b075
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-08-17 18:42:20 +0200

    Streamlining

--- main.cpp
+++ main.cpp
@@ -130,7 +130,7 @@ int execute(string data, bool objdump, bool verbose){
         cout<<"--------";
     }
     
-    cout<<"\n"<<b-a<<" ns";
+    //cout<<"\n"<<b-a<<" ns";
     if (returned==CALL_ERR || returned==NULL){
         return -1;
     }
