afda8ea68b848924d5e20096426dea83db8e8416
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-08-18 02:56:24 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -36,6 +36,30 @@ using namespace std;
 
 string program;
 
+string FPL_LICENSE=R""""(
+MIT License
+
+Copyright (c) 2022 Eric Buehler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+)"""";
+
 #include "utilities/utils.h"
 #include "lexer/lexer.h"
 #include "parser/parser.h"
@@ -239,6 +263,14 @@ int main(int argc, char** argv) {
             cout<<"Object dump run: fpl [PROGRAM NAME] -o\n";
             cout<<"Enter REPL: fpl \n";
             cout<<"Help: fpl -h\n";
+            cout<<"Licenses: fpl -l\n";
+            return 0;
+        }
+        if ((string)argv[1]==(string)"-l"){
+            cout<<"FPL License\n";
+            cout<<FPL_LICENSE;
+            cout<<"\n\n-------------------------------------\n\nBigFloat License\n";
+            cout<<FPL_BigFloat_LICENSE;
             return 0;
         }
         program=argv[1];
