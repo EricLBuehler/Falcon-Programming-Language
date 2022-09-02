16042b4b81568a6f91e1af90d937dea853082f5b
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-09-01 20:34:15 -0400

    Update

--- compiler/compiler.h
+++ compiler/compiler.h
@@ -124,6 +124,7 @@ struct compiler{
 #define STSLICE(node) ((StoreSlice*)node)
 #define DEL(node) ((Del*)node)
 #define GLBLIDENT(node) ((GlblIdent*)node)
+#define NLIDENT(node) ((NonlocalIdent*)node)
 
 #define NAMEIDX(obj) (*CAST_INT(obj->type->slot_mappings->slot_len(obj))->val).to_long_long()-1
 
