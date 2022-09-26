ae7d8143a6b69852c078029b3a48ba4030a748da
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-09-26 15:02:17 -0400

    Speed enhancements

--- vm/vm.cpp
+++ vm/vm.cpp
@@ -1573,14 +1573,21 @@ object* run_vm(object* codeobj, uint32_t* ip){
     size_t len=CAST_LIST(lines)->size-1;
     
     vm->callstack->head->line=list_index_int(linetup, 2);
+
     while ((*ip)<instructions){
         instruction=list_index_int(code, (*ip)++);
+
         if (((*ip)-1)/2>=(*CAST_INT(list_index_int(linetup, 1))->val)){
             linetup=list_index_int(lines, linetup_cntr++);
             vm->callstack->head->line=list_index_int(linetup, 2);
         }
 
         object* obj=_vm_step(instruction, list_index_int(code, (*ip)++), vm, ip, &linetup_cntr, linetup);
+        
+        if (obj==NULL){
+            continue;
+        }
+
         if (obj==TERM_PROGRAM){
             return TERM_PROGRAM;
         }
