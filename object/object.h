4d98c53c8b43592ec5badc6c503ab207a95a433b
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-10-02 08:52:55 -0400

    Added isspace, isalpha, isnumeric

--- object/object.h
+++ object/object.h
@@ -137,7 +137,7 @@ static object* trueobj=NULL;
 static object* falseobj=NULL;
 static object* noneobj=NULL;
 
-const size_t nbuiltins=51;
+const size_t nbuiltins=54;
 object* builtins[nbuiltins];
 
 TypeObject TypeError;
