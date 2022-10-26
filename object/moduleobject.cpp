a8fb8e226d19ba837f55e4d19430d8fdc9c6c05d
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-10-26 08:11:19 -0400

    Weak ref update

--- object/moduleobject.cpp
+++ object/moduleobject.cpp
@@ -3,10 +3,11 @@ void module_del(object* self){
     FPLDECREF(CAST_MODULE(self)->name);
 }
 
+//TAKES WEAK REFs
 object* module_new_fromdict(object* dict, object* name){
     object* m=new_object(&ModuleType);
-    CAST_MODULE(m)->dict=FPLINCREF(dict);
-    CAST_MODULE(m)->name=FPLINCREF(name);
+    CAST_MODULE(m)->dict=dict;
+    CAST_MODULE(m)->name=name;
     return m;
 }
 
