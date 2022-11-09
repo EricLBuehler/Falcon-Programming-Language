cc0167f67d57981b80cceb905db7bb1871c92a8a
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-11-09 06:06:31 -0500

    String comparison optimization

--- object/object.cpp
+++ object/object.cpp
@@ -228,7 +228,7 @@ bool object_find_bool(object* iter, object* needle){
 
 bool object_find_bool_dict_keys(object* dict, object* needle){
     for (auto k: (*CAST_DICT(dict)->val)){
-        if (istrue(object_cmp(k.first,needle, CMP_EQ))){
+        if (*CAST_STRING(k.first)->val==*CAST_STRING(needle)->val){
             return true;
         }
     }
@@ -237,7 +237,7 @@ bool object_find_bool_dict_keys(object* dict, object* needle){
 
 object* object_find_dict_keys(object* dict, object* needle){
     for (auto k: (*CAST_DICT(dict)->val)){
-        if (istrue(object_cmp(k.first,needle, CMP_EQ))){
+        if (*CAST_STRING(k.first)->val==*CAST_STRING(needle)->val){
             return k.first;
         }
     }
