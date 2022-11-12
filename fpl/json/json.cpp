71487b9520490b87940c6fb7b4c54014a0879c4b
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-11-12 09:02:41 -0500

    Memory leak bug fix and update

--- fpl/json/json.cpp
+++ fpl/json/json.cpp
@@ -41,25 +41,25 @@ object* json_decode_object(const nx_json* json){
     while (item){
         switch (item->type){
             case NX_JSON_NULL:
-                dict_set(dict, str_new_fromstr(string(item->key)), new_none());
+                dict_set_noret(dict, str_new_fromstr(string(item->key)), new_none());
                 break;
             case NX_JSON_STRING:
-                dict_set(dict, str_new_fromstr(string(item->key)), str_new_fromstr(string(item->text_value)));
+                dict_set_noret(dict, str_new_fromstr(string(item->key)), str_new_fromstr(string(item->text_value)));
                 break;
             case NX_JSON_INTEGER:
-                dict_set(dict, str_new_fromstr(string(item->key)), new_int_fromint(item->num.s_value));
+                dict_set_noret(dict, str_new_fromstr(string(item->key)), new_int_fromint(item->num.s_value));
                 break;
             case NX_JSON_DOUBLE:
-                dict_set(dict, str_new_fromstr(string(item->key)), new_float_fromdouble(item->num.dbl_value));
+                dict_set_noret(dict, str_new_fromstr(string(item->key)), new_float_fromdouble(item->num.dbl_value));
                 break;
             case NX_JSON_BOOL:
-                dict_set(dict, str_new_fromstr(string(item->key)), item->num.u_value? new_bool_true() : new_bool_false());
+                dict_set_noret(dict, str_new_fromstr(string(item->key)), item->num.u_value? new_bool_true() : new_bool_false());
                 break;
             case NX_JSON_OBJECT:
-                dict_set(dict, str_new_fromstr(string(item->key)), json_decode_object(item));
+                dict_set_noret(dict, str_new_fromstr(string(item->key)), json_decode_object(item));
                 break;
             case NX_JSON_ARRAY:
-                dict_set(dict, str_new_fromstr(string(item->key)), json_decode_array(item));
+                dict_set_noret(dict, str_new_fromstr(string(item->key)), json_decode_array(item));
                 break;
         }
         item=item->next;
@@ -71,6 +71,7 @@ object* json_decode(object* selftp, object* args){
     object* str=dict_get(args, str_new_fromstr("string"));
 
     const nx_json* json=nx_json_parse_utf8((char*)object_cstr(str).c_str());
+    FPLDECREF(str);
     if (json!=NULL){
         return json_decode_object(json);
     }
@@ -194,10 +195,15 @@ object* json_encode(object* selftp, object* args){
 
     if (!object_istype(head->type, &DictType)){
         vm_add_err(&TypeError, vm, "Expected dict, got '%s'", head->type->name->c_str());
+        FPLDECREF(head);
         return NULL; 
     }
+
+    object* d=json_encode_dict(head);
+
+    FPLDECREF(head);
     
-    return json_encode_dict(head);
+    return d;
 }
 
 object* new_json_module(){
@@ -208,13 +214,13 @@ object* new_json_module(){
     object* decodeargs=new_tuple();
     decodeargs->type->slot_mappings->slot_append(decodeargs, str_new_fromstr("string"));
     object* decode=new_builtin(json_decode, str_new_fromstr("decode"), decodeargs, emptykwargs, 1, false);
-    dict_set(dict, str_new_fromstr("decode"), decode);
+    dict_set_noret(dict, str_new_fromstr("decode"), decode);
     FPLDECREF(decode);
 
     object* encodeargs=new_tuple();
     encodeargs->type->slot_mappings->slot_append(encodeargs, str_new_fromstr("object"));
     object* encode=new_builtin(json_encode, str_new_fromstr("encode"), encodeargs, emptykwargs, 1, false);
-    dict_set(dict, str_new_fromstr("encode"), encode);
+    dict_set_noret(dict, str_new_fromstr("encode"), encode);
     FPLDECREF(encode);
 
     return module_new_fromdict(dict, str_new_fromstr("json"));
