53d5d25616105ea176ddf2d9f92a36e5bf6cd46a
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-11-23 18:01:03 -0500

    Add unicode support

--- utilities/utils.h
+++ utilities/utils.h
@@ -4,6 +4,9 @@
 
 void memory_error();
 char int_base_check(string s, int base);
+int decode_code_point(char** s);
+void encode_code_point(char **s, char *end, int code);
+string codept_to_str(uint32_t num);
 #include "memory.cpp"
 
 #ifndef NULL
