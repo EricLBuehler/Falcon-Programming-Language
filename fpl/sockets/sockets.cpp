b1c7313a052d887e8e4e76d1ed9d326e11792830
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-11-25 05:51:50 -0500

    Rearrange includes

--- fpl/sockets/sockets.cpp
+++ fpl/sockets/sockets.cpp
@@ -3,7 +3,6 @@
   #ifndef _WIN32_WINNT
     #define _WIN32_WINNT 0x0501  /* Windows XP. */
   #endif
-  #include <winsock2.h>
   #include <Ws2tcpip.h>
 #else
   /* Assume that any non-Windows platform uses POSIX-style sockets instead. */
