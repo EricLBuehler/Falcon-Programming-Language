84a8c021aa12c31739120f5566d930463e1e7676
Eric Buehler <65165915+EricLBuehler@users.noreply.github.com>
2022-11-30 14:56:05 -0500

    __forceinline for GIL

--- gil/gil.cpp
+++ gil/gil.cpp
@@ -1,9 +1,9 @@
-void gil_lock(int id){
+__forceinline void gil_lock(int id){
     _gil.lock();
     ::vm=interpreter.vm_map->at(id);
 }
 
-int gil_unlock(){
+__forceinline int gil_unlock(){
     uint32_t id=vm->id;
     _gil.unlock();
     return id;
